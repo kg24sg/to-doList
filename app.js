4b31742173bf4db2edac8b5961f5186eb87c2887
Constantinos Gregoriou <konstantinos.24@hotmail.com>
2022-05-31 22:16:42 +0300

    !!

--- app.js
+++ app.js
@@ -33,41 +33,84 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-if(err){
-  console.log(err);
-}else{
-  console.log("Succesfully saved default items to DB.");
-}
-});
+const listSchema ={
+  name: String,
+  items: [itemsSchema]
+};
+
+const List = mongoose.model("List", listSchema);
 
 app.get("/", function(req, res) {
 
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+
+  Item.find({},function(err,foundItems){
+
+    if (foundItems.length === 0){
+      Item.insertMany(defaultItems, function(err){
+      if(err){
+        console.log(err);
+      }else{
+        console.log("Succesfully saved default items to DB.");
+      }
+      });
+      res.redirect("/")
+    }else {
+        res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
+
+
+  });
 
 });
 
 app.post("/", function(req, res){
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
+  const item = new Item({
+    name: itemName
+  });
+
+  item.save();
+  res.redirect("/");
+});
+
+app.post("/delete", function(req, res){
+const checkedItemId = req.body.checkbox;
+Item.findByIdAndRemove(checkedItemId,function(err){
+if(err){
+  console.log(err);
+}else{
+  console.log("Succesfully deleted  item from DB.");
+}
+});
+res.redirect("/");
+});
+
+app.get("/:customListName", function(req,res){
+  const customListName = req.params.customListName;
+
+List.findOne({name: customListName}, function(err, foundList){
+  if(!err){
+    if(!foundList){
+      const list = new List({
+        name:customListName,
+        items:defaultItems
+      });
+
+      list.save();
+      res.redirect("/"+customListName);
+    }else {
+      res.render("list",{listTitle: foundList.name, newListItems: foundList.items})
+    }
   }
+
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+
 });
 
-app.get("/about", function(req, res){
-  res.render("about");
-});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
